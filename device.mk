7511861159b465b5b3ea15f80cb51a918ca629d0
kuroringo90 <ak4700@gmx.com>
2024-10-14 21:49:57 +0200

    tmp

--- device.mk
+++ device.mk
@@ -532,11 +532,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Vndservicemanager
 PRODUCT_PACKAGES += \
